a454ef169b14872b6674adc13ceca02391bb23f7
rig <rihard.rivis@proton.me>
2023-08-12 10:37:06 +0000

    added category id finder and number of categorys

---
+++ src/rimi/rimi.py
@@ -0,0 +1,14 @@
+"""Rimi webpage scraper"""
+
+base_url = "https://www.rimi.ee/epood"
+
+# id of ul element containing category links
+
+
+def category_id(n): return f"desktop_category_menu_{n}"
+
+
+# 1 indexed (1..19)
+number_of_categories = 19
+
+# ul[category_id] -> (first)li -> a[href]
