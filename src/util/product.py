63606f5d5621fc0832868f03f4553b83a42daa00
rig <rihard.rivis@proton.me>
2023-08-12 12:28:30 +0000

    product info helper class added

--- src/util/product.py
+++ src/util/product.py
@@ -1,8 +1,30 @@
-class RimiProduct:
-    def __init__(self, name, price_per_product, price_per_unit, unit):
+"""Product helpers"""
+
+from typing import TypedDict
+
+
+class PricePerUnit(TypedDict):
+    """Price per unit helper class"""
+    price: int
+    unit: str
+
+
+class Product:
+    """Product helper class"""
+
+    def __init__(self, name: str, price: int, price_per_unit: PricePerUnit, category: str):
         self.name = name
-        # price if you buy 1 product
-        self.price_per_produt = price_per_product
-        # price if you buy 1 unit (e.g. 1kg)
-        self.price_per_unit = price_per_unit
-        self.unit = unit
+        # price that is paid for 1 product
+        self.price = price
+        # price that is paid for 1 unit of product (e.g. 1kg)
+        self.price_per_unit = price_per_unit['price']
+        self.unit = price_per_unit['unit']
+        self.category = category
+        self.weight = price * 100 // self.price_per_unit
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def price_string(self):
+        """Return price as a string"""
+        return f"{self.price}"
