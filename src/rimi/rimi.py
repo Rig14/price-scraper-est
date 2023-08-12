a7b6c4828abfbe10a03b7d572ba2281770da4d18
rig <rihard.rivis@proton.me>
2023-08-12 11:14:44 +0000

    added a function to get category urls

--- src/rimi/rimi.py
+++ src/rimi/rimi.py
@@ -1,16 +1,27 @@
 """Rimi webpage scraper"""
 
 from bs4 import BeautifulSoup
-
-base_url = "https://www.rimi.ee/epood"
-
-# id of ul element containing category links
+import requests
 
 
-def category_id(n): return f"desktop_category_menu_{n}"
-
-
+BASE_URL = "https://www.rimi.ee"
+E_STORE_URL = BASE_URL + "/epood"
+# id of the ul element containing the category item
+CATEGORY_ID = "desktop_category_menu_"
 # 1 indexed (1..19)
-number_of_categories = 19
+CATEGORY_N = 19
 
-# ul[category_id] -> (first)li -> a[href]
+
+def get_category_urls():
+    """Return a list of category urls"""
+    category_urls = []
+    html = requests.get(E_STORE_URL, timeout=10).text
+    soup = BeautifulSoup(html, "html.parser")
+
+    # ul[category_id] -> (first)li -> a[href]
+    for index in range(1, CATEGORY_N+1):
+        ul_element = soup.find("ul", {"id": CATEGORY_ID + str(index)})
+        li_element = ul_element.find("li")
+        link = li_element.find("a")
+        category_urls.append(BASE_URL + link["href"])
+    return category_urls
